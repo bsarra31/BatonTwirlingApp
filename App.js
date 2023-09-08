c1dadf62c42e6f28ff513a98f7c37e8856d8053b
bsarra31 <bsarra31@gmail.com>
2023-09-07 20:11:53 -0400

    I dont think anything changed here LMAO.

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import CreateAccount from './src/screens/CreateAccount';
 import Login from './src/screens/Login';
-//That is bower's comment.
-//There is bower's comment.
+
 const App = () => {
   return (
     <View style={styles.container}>
-      <CreateAccount />
-      {/* <Login /> */}
+      {/* <CreateAccount /> */}
+      <Login />
     </View>
   );
 }
