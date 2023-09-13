141d1bc3098fe46bd2324b6293982214175789ba
bsarra31 <bsarra31@gmail.com>
2023-09-12 22:58:54 -0400

    Created UserInput component for custom input fields

---
+++ src/components/UserInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
+import Texts from '../styles/Texts';
+
+const UserInput = (props) => {
+
+    const { placeHolder, keyboardType } = props;
+    if (keyboardType === 'Password') {
+
+    }
+    return (
+        <TextInput
+            style={Texts.input}
+            placeholder={placeHolder}
+            placeholderTextColor={'#C0BFC7'}
+            keyboardType={keyboardType}
+            secureTextEntry={placeHolder === 'Password' ? true : false}
+        />
+    )
+};
+
+export default UserInput;
