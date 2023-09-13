4afae9553290333509199be6d7774b44254e7baf
bsarra31 <bsarra31@gmail.com>
2023-09-13 18:23:37 -0400

    Creating test tab navigation container

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,31 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Login from "../screens/Login";
+import CreateAccount from "../screens/CreateAccount";
+
+//TAB BOTTOM
+const Tab = createBottomTabNavigator();
+
+function TabGroup() {
+    return(
+        
+        <Tab.Navigator>
+            <Tab.Screen 
+                name='Login'
+                component={Login}
+            />
+            <Tab.Screen 
+                name='Signup'
+                component={CreateAccount}
+            />
+        </Tab.Navigator>
+    )
+};
 
 export default function Navigation () {
     return (
         <NavigationContainer>
-            
+            <TabGroup />
         </NavigationContainer>
     )
 };
