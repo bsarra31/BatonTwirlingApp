8e10b5be7ff25a47a7816e19a1d72f0a8164a034
bsarra31 <bsarra31@gmail.com>
2023-09-12 23:00:14 -0400

    Updating to make text scalable across devices

--- src/styles/Texts.js
+++ src/styles/Texts.js
@@ -1,27 +1,36 @@
-import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
-const texts = StyleSheet.create({
+const entireScreenWidth = Dimensions.get('window').width;
+EStyleSheet.build({ $rem: entireScreenWidth / 390 });
+
+const texts = EStyleSheet.create({
     titleText: {
         color: '#611f66',
-        fontSize: 32,
+        fontSize: '32rem',
         fontWeight: 'bold',
     },
     buttonText: {
         color: '#f7f0ea',
-        fontSize: 16,
+        fontSize: '16rem',
         fontWeight: 'bold',
     },
     descriptionText: {
         color: '#197278',
-        fontSize: 16,
+        fontSize: '16rem',
     },
     input: {
         borderColor: '#000000',
-        borderRadius: 6,
-        borderWidth: 1,
-        fontSize: 16,
-        height: 48,
-        paddingLeft: 10,
+        borderRadius: '6rem',
+        borderWidth: '1rem',
+        fontSize: '16rem',
+        height: '48rem',
+        paddingLeft: '10rem',
+    },
+    linkText: {
+        color: '#197278',
+        fontSize: '16rem',
+        textDecorationLine: 'underline',
     },
 });
 
