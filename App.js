39df63b151f4147bacb1ce0191af53291b9d41e4
bsarra31 <bsarra31@gmail.com>
2023-09-13 18:11:42 -0400

    Moving navigation to it's own component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 //IMPORTS
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from './src/screens/Login';
 import CreateAccount from './src/screens/CreateAccount';
+import Navigation from './src/components/Navigation';
 
 //STACK NAVIGATOR CREATION
 const Stack = createNativeStackNavigator();
@@ -13,18 +14,19 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   //MAIN JSX
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          name="Sign Up"
-          component={CreateAccount}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Navigation />
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen
+    //       name="Login"
+    //       component={Login}
+    //     />
+    //     <Stack.Screen
+    //       name="Sign Up"
+    //       component={CreateAccount}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 };
 
